1dc114ce0c104492a6d10f439d990fa0fbae17a9
HuiLee <lihui870920@gmail.com>
2025-07-18 11:56:03 +0800

    系统工具类Hutool

--- src/main/java/com/julan/tools/controller/ToolController.java
+++ src/main/java/com/julan/tools/controller/ToolController.java
@@ -1,13 +1,10 @@
 package com.julan.tools.controller;
 
-
+import cn.hutool.core.util.IdUtil;
 import com.julan.tools.request.validated.ValidReq;
 import com.julan.tools.util.api.ResultJson;
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("tool")
@@ -17,6 +14,12 @@ public class ToolController {
     @PostMapping("valid")
     public ResultJson<ValidReq> valid(@Valid @RequestBody ValidReq validReq) {
         return ResultJson.success(validReq);
+    }
+
+
+    @GetMapping("hutool")
+    public ResultJson<Object> hutool() {
+        return ResultJson.success(IdUtil.objectId());
     }
 
 }
